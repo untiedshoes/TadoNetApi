4d6157eda49fa98960ec46b7191784011e3434b4
Craig Richards <geraldrichards@Craigs-MacBook-Pro.local>
2026-04-02 17:37:14 +0100

    refactor: move device component mappings from ZoneMapper to DeviceMapper

--- src/TadoNetApi.Infrastructure/Mappers/ZoneMapper.cs
+++ src/TadoNetApi.Infrastructure/Mappers/ZoneMapper.cs
@@ -34,30 +34,6 @@ namespace TadoNetApi.Infrastructure.Mappers
         }
 
         /// <summary>
-        /// Maps a <see cref="TadoCharacteristicsResponse"/> to a <see cref="Characteristics"/> domain entity.
-        /// </summary>
-        /// <param name="dto">The API DTO to map.</param>
-        /// <returns>A mapped <see cref="Characteristics"/>.</returns>
-        public static Characteristics ToDomain(this TadoCharacteristicsResponse dto)
-            => new() { Capabilities = dto.Capabilities };
-
-        /// <summary>
-        /// Maps a <see cref="TadoMountingStateResponse"/> to a <see cref="MountingState"/> domain entity.
-        /// </summary>
-        /// <param name="dto">The API DTO to map.</param>
-        /// <returns>A mapped <see cref="MountingState"/>.</returns>
-        public static MountingState ToDomain(this TadoMountingStateResponse dto)
-            => new() { Value = dto.Value, Timestamp = dto.Timestamp };
-
-        /// <summary>
-        /// Maps a <see cref="TadoConnectionStateResponse"/> to a <see cref="ConnectionState"/> domain entity.
-        /// </summary>
-        /// <param name="dto">The API DTO to map.</param>
-        /// <returns>A mapped <see cref="ConnectionState"/>.</returns>
-        public static ConnectionState ToDomain(this TadoConnectionStateResponse dto)
-            => new() { Value = dto.Value, Timestamp = dto.Timestamp };
-
-        /// <summary>
         /// Maps a <see cref="TadoDazzleModeResponse"/> to a <see cref="DazzleMode"/> domain entity.
         /// </summary>
         /// <param name="dto">The API DTO to map.</param>
